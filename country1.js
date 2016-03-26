a153dee9901e9349abfb1bd13b616a6bde5ecc32
imran-mind <shaikhimran115@gmail.com>
2016-03-26 14:19:08 +0530

    country1.js is added in repository

---
+++ country1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by root on 25/3/16.
+ */
+
+var country = require('countryjs');
+//console.log(country.states('IND', 'ISO3')); // 'ISO2', 'ISO3', 'name'
+console.log(country.states('IN')); // Defaults to ISO2
+console.log('Hello h ru ')
