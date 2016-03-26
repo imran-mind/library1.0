037da6b47d49254a0b21cae2309c31b5d91034f2
imran-mind <shaikhimran115@gmail.com>
2016-03-26 16:53:15 +0530

    changes in country1.js

--- country1.js
+++ country1.js
@@ -5,4 +5,5 @@
 var country = require('countryjs');
 //console.log(country.states('IND', 'ISO3')); // 'ISO2', 'ISO3', 'name'
 console.log(country.states('IN')); // Defaults to ISO2
-console.log('Hello h ru ')
+console.log('Hello h ru ');
+console.log('spreading happiness');
